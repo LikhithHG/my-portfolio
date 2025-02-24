7b84262dc1e56ea43b942eb54394ab7b9635bfa8
Likhith H G <likhith2098@gmail.com>
2025-02-23 22:30:34 -0700

    Image folder

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -2,9 +2,42 @@ import { FaGithub, FaHandsPraying, FaLinkedin } from "react-icons/fa6";
 import { FaRegHandshake, FaOm } from "react-icons/fa";
 import { GiShintoShrine } from "react-icons/gi";
 import { SiLeetcode, } from "react-icons/si";
-import myportfolio from "../assets/myPortFolio.png";
+import about from "../assets/about.jpeg";
+import aws from "../assets/aws.png";
+import cerner from "../assets/cerner.png";
+import cernerOffer from "../assets/CernerOffer.jpg";
+import computerNetwork from "../assets/computernetwork.png";
+import css from "../assets/css.svg";
+import cubical from "../assets/cubical.jpg";
+import cudenver from "../assets/cudenver.png";
+import cyberSecurity from "../assets/cybersecurity.png";
+import daveOgle from "../assets/DaveOgle.jpg";
 import ganeshSamiti from "../assets/ganeshSamiti.png";
-
+import git from "../assets/git.svg";
+import html5 from "../assets/html5.svg";
+import internshipDinner from "../assets/Internship Dinner.jpg";
+import internship1 from "../assets/Internship1.jpg";
+import java from "../assets/java.png";
+import javascript from "../assets/javascript.svg";
+import mongodb from "../assets/mongodb.svg";
+import myportfolio from "../assets/myPortFolio.png";
+import mysql from "../assets/mysql.png";
+import nextjs from "../assets/nextjs.png";
+import nodejs from "../assets/nodejs.svg";
+import office from "../assets/Office.jpg";
+import oops from "../assets/oops.png";
+import oracleCerner from "../assets/oraclecerner.png";
+import python from "../assets/python.svg";
+import react from "../assets/react.svg";
+import reactNative from "../assets/reactNative.png";
+import redux from "../assets/redux.svg";
+import sdlc from "../assets/sdlc.png";
+import sit from "../assets/sit.png";
+import taClass1 from "../assets/TA Class 1.jpg";
+import taClass2 from "../assets/TA Class 2.jpg";
+import taClass3 from "../assets/TA Class 3.jpg";
+import tailwindcss from "../assets/tailwindcss.svg";
+import typescript from "../assets/typescript.svg";
 
 export const greetings = [
     { text: "Hello", icon: <FaRegHandshake className="text-blue-500" /> },
@@ -54,7 +87,7 @@ export const educationInformation = [
         startYear: "Aug  2023",
         endYear: "May 2025",
         accomplishment: "Teaching Assistant under Prof. David Ogle",
-        logo: "src/assets/cudenver.png",
+        logo: cudenver,
     },
     {
         university: "Siddaganga Institute of Technology",
@@ -62,38 +95,38 @@ export const educationInformation = [
         startYear: "Aug 2016 ",
         endYear: "May 2020",
         accomplishment: "Gold Medalist for highest GPA",
-        logo: "src/assets/sit.png",
+        logo: sit,
     },
 ];
 
 export const skills = [
-    { title: 'JavaScript', level: 'Advanced', image: 'src/assets/javascript.svg' },
-    { title: 'React.JS', level: 'Advanced', image: 'src/assets/react.svg' },
-    { title: 'Node.JS', level: 'Intermediate', image: 'src/assets/nodejs.svg' },
-    { title: 'Python', level: 'Intermediate', image: 'src/assets/python.svg' },
-    { title: 'Java', level: 'Good', image: 'src/assets/java.png' },
-    { title: 'TypeScript', level: 'Intermediate', image: 'src/assets/typescript.svg' },
-    { title: 'HTML', level: 'Advanced', image: 'src/assets/html5.svg' },
-    { title: 'CSS', level: 'Intermediate', image: 'src/assets/css.svg' },
-    { title: 'TailwindCSS', level: 'Intermediate', image: 'src/assets/tailwindcss.svg' },
-    { title: 'ReactRedux', level: 'Intermediate', image: 'src/assets/redux.svg' },
-    { title: 'ReactNative', level: 'Intermediate', image: 'src/assets/reactNative.png' },
-    { title: 'NextJS', level: 'Intermediate', image: 'src/assets/nextjs.png' },
-    { title: 'MySQL', level: 'Intermediate', image: 'src/assets/mysql.png' },
-    { title: 'MongoDB', level: 'Intermediate', image: 'src/assets/mongodb.svg' },
-    { title: 'Git', level: 'Advanced', image: 'src/assets/git.svg' },
-    { title: 'OOPs', level: 'Advanced', image: 'src/assets/oops.png' },
-    { title: 'Computer Network', level: 'Advanced', image: 'src/assets/computernetwork.png' },
-    { title: 'CyberSecurity', level: 'Intermediate', image: 'src/assets/cybersecurity.png' },
-    { title: 'AWS', level: 'Intermediate', image: 'src/assets/aws.png' },
-    { title: 'SDLC', level: 'Intermediate', image: 'src/assets/sdlc.png' }, 
+    { title: 'JavaScript', level: 'Advanced', image: javascript },
+    { title: 'React.JS', level: 'Advanced', image: react },
+    { title: 'Node.JS', level: 'Intermediate', image: nodejs },
+    { title: 'Python', level: 'Intermediate', image: python },
+    { title: 'Java', level: 'Good', image: java },
+    { title: 'TypeScript', level: 'Intermediate', image: typescript },
+    { title: 'HTML', level: 'Advanced', image: html5 },
+    { title: 'CSS', level: 'Intermediate', image: css },
+    { title: 'TailwindCSS', level: 'Intermediate', image: tailwindcss },
+    { title: 'ReactRedux', level: 'Intermediate', image: redux },
+    { title: 'ReactNative', level: 'Intermediate', image: reactNative },
+    { title: 'NextJS', level: 'Intermediate', image: nextjs },
+    { title: 'MySQL', level: 'Intermediate', image: mysql },
+    { title: 'MongoDB', level: 'Intermediate', image: mongodb },
+    { title: 'Git', level: 'Advanced', image: git },
+    { title: 'OOPs', level: 'Advanced', image: oops },
+    { title: 'Computer Network', level: 'Advanced', image: computerNetwork },
+    { title: 'CyberSecurity', level: 'Intermediate', image: cyberSecurity },
+    { title: 'AWS', level: 'Intermediate', image: aws },
+    { title: 'SDLC', level: 'Intermediate', image: sdlc }, 
 ];
 
 export const experience = [
     {
         title: "Teaching Assistant",
         company: "University of Colorado Denver",
-        logo: "src/assets/cudenver.png",
+        logo: cudenver,
         duration: "Jan 2024 - Present",
         description: "Mentored students, graded assignments, and conducted tutorial sessions under Prof. Dave Ogle.",
         galleryLink: "/gallery",
@@ -101,7 +134,7 @@ export const experience = [
     {
         title: "Network Software Engineer",
         company: "Oracle Cerner",
-        logo: "src/assets/oraclecerner.png",
+        logo: oracleCerner,
         duration: "Jun 2020 - Jul 2023",
         description: "Developed and optimized software and network automation solutions for healthcare applications.",
         galleryLink: "/gallery",
@@ -109,7 +142,7 @@ export const experience = [
     {
         title: "Software - System Intern",
         company: "Cerner",
-        logo: "src/assets/cerner.png",
+        logo: cerner,
         duration: "Jan 2020 - Jun 2020",
         description: "Worked on real-time data analytics for healthcare applications, improving data processing speeds.",
         galleryLink: "/gallery",
@@ -120,55 +153,55 @@ export const galleryImages = [
     {
         id: 1,
         category: "Teaching Assistant",
-        src: "src/assets/DaveOgle.jpg",
+        src: daveOgle,
         alt: "Image"
     },
     {
         id: 2,
         category: "Teaching Assistant",
-        src: "src/assets/TA Class 1.jpg",
+        src: taClass1,
         alt: "Image"
     },
     {
         id: 3,
         category: "Teaching Assistant",
-        src: "src/assets/TA Class 2.jpg",
+        src: taClass2,
         alt: "Image"
     },
     {
         id: 4,
         category: "Teaching Assistant",
-        src: "src/assets/TA Class 3.jpg",
+        src: taClass3,
         alt: "Image"
     },
     {
         id: 5,
         category: "Network Software Engineer",
-        src: "src/assets/Office.jpg",
+        src: office,
         alt: "Network Engineer 2"
     },
     {
         id: 6,
         category: "Network Software Engineer",
-        src: "src/assets/cubical.jpg",
+        src: cubical,
         alt: "Network Engineer 2"
     },
     {
         id: 7,
         category: "Internship",
-        src: "src/assets/CernerOffer.jpg",
+        src: cernerOffer,
         alt: "Internship"
     },
     {
         id: 8,
         category: "Internship",
-        src: "src/assets/Internship Dinner.jpg",
+        src: internshipDinner,
         alt: "Internship"
     },
     {
         id: 9,
         category: "Internship",
-        src: "src/assets/Internship1.jpg",
+        src: internship1,
         alt: "Internship"
     },
 ];
